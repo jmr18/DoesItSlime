7eeaff51bc17031c61f61bc62b1237a251881668
J R <laxstar1996@gmail.com>
2025-01-12 14:15:07 -0500

    Wording changes.

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -21,10 +21,10 @@ function search_slimage() {
 </script>
 
 <template>
-  <h2>Test page for DoesItSlime.</h2>
+  <h2>DoesItSlime?</h2>
   <div>
     <span><input type="text" id="search-box"/></span>
-    <span><button v-on:click="search_slimage()">Slime?</button></span>
+    <span><button v-on:click="search_slimage()">Slime</button></span>
   </div>
   <div v-if="search_param !== ''">
     <span v-if="does_it_slime"> {{ search_param }} does slime!</span>
