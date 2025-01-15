b9055337dc3d5eb971032e7e915c030ccce0a294
J R <laxstar1996@gmail.com>
2025-01-14 19:05:53 -0500

    Slime style initiated.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 -[X] Return random slime value \
 -[X] Deploy to Github \
 -[X] Deploy to Vercel \
--[] Style components \
+-[X] Style components \
 -[] Add a database with sample data \
 -[] Route to allow users to add topic and initial sentiment \
 -[] Return slime sentiment data from database
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -21,12 +21,13 @@ function search_slimage() {
 </script>
 
 <template>
-  <h2>DoesItSlime?</h2>
-  <div>
+  <div id="banner">Does It Slime?</div>
+  <div id="search-section">
     <span><input type="text" id="search-box"/></span>
     <span><button v-on:click="search_slimage()">Slime</button></span>
   </div>
-  <div v-if="search_param !== ''">
+  <br />
+  <div id="result-section" v-if="search_param !== ''">
     <span v-if="does_it_slime"> {{ search_param }} does slime!</span>
     <span v-else> {{ search_param }} does not slime...</span>
   </div>
